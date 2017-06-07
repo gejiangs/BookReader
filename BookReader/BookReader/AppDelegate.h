c500f5b93781b11edb90d6438e7ba2b06228be1f
郭江 <664553782@qq.com>
2017-06-07 14:30:17 +0800

    添加项目基本文件

---
+++ BookReader/BookReader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BookReader
+//
+//  Created by 郭江 on 2017/6/7.
+//  Copyright © 2017年 郭江. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BookReader/BookReader/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  BookReader
+//
+//  Created by 郭江 on 2017/6/7.
+//  Copyright © 2017年 郭江. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
